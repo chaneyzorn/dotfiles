871d36199fc82166b821d53c63be1ba6cc9f2433
chaney <chaneyzorn@gmail.com>
2018-07-09 00:05:34 +0800

    update screenshot.sh

--- myscripts/bin/screenshot.sh
+++ myscripts/bin/screenshot.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 maim -s -u -b 3.5 -c 0.45,0.7,1,0.2 -l /tmp/main-screenshot.png
 if [ -f /tmp/main-screenshot.png ]; then
-    echo 'maim: screenshot successed!'
     xclip -selection c -t image/png /tmp/main-screenshot.png
-    mv /tmp/main-screenshot.png /home/chaney/Desktop/`date +%Y-%m-%d-%H:%M:%S`-screenshot.png
+    img_name=/home/chaney/Desktop/`date +%Y-%m-%d-%H:%M:%S`-screenshot.png
+    mv /tmp/main-screenshot.png $img_name
+    notify-send -i $img_name 'maim' 'screenshot successed!\nimage has been save to '$img_name
 else
-    echo 'maim: screenshot cancelled!'
+    notify-send 'maim' 'screenshot cancelled!'
 fi
+
