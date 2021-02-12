9f93c7fd4710e45e333810e5c40833b3e6021162
chaney <chaneyzorn@gmail.com>
2021-02-12 18:51:53 +0800

    nvim: disable better_whitespace map

--- nvim/.config/nvim/lua/neo/preplug.lua
+++ nvim/.config/nvim/lua/neo/preplug.lua
@@ -84,6 +84,8 @@ end)("sheerun/vim-polyglot");
   -- 使用 g:better_whitespace_filetypes_blacklist 无效
   -- https://github.com/ntpeters/vim-better-whitespace/issues/108
   vim.cmd [[ autocmd FileType help DisableWhitespace ]]
+  -- 禁用默认 keymap
+  vg.better_whitespace_operator = ""
 end)("ntpeters/vim-better-whitespace");
 
 (function()
