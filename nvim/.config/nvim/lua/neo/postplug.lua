1d5505a3f2ea2c88f81977e5a11528b37a9daff3
chaney <chaneyzorn@gmail.com>
2021-04-28 21:27:29 +0800

    nvim bufferline disable enforce_regular_tabs

--- nvim/.config/nvim/lua/neo/postplug.lua
+++ nvim/.config/nvim/lua/neo/postplug.lua
@@ -90,7 +90,7 @@ end)('build-in terminal color');
       show_tab_indicators = true,
       persist_buffer_sort = true,
       separator_style = "thin",
-      enforce_regular_tabs = true,
+      enforce_regular_tabs = false,
       always_show_bufferline = false,
       sort_by = 'directory',
     },
