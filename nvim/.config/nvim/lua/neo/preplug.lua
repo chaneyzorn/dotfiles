8e5fcc09d8dedd886aefa9217533481d754fd249
chaney <chaneyzorn@gmail.com>
2021-06-10 17:51:46 +0800

    nvim: update spell and lint config

--- nvim/.config/nvim/lua/neo/preplug.lua
+++ nvim/.config/nvim/lua/neo/preplug.lua
@@ -114,8 +114,8 @@ end)("luochen1990/rainbow");
 end)("jiangmiao/auto-pairs");
 
 (function()
-  vg.indent_blankline_char = '|'
-  vg.indent_blankline_char_list = {'|', '¦', '┆', '┊'}
+  vg.indent_blankline_char = '┊'
+  -- vg.indent_blankline_char_list = {'|', '¦', '┆', '┊'}
   -- 以下类型的 buffer 里不显示缩进线
   vg.indent_blankline_filetype_exclude = {"help", "terminal", "list", "nofile"}
   vg.indent_blankline_buftype_exclude = {"help", "terminal", "list", "nofile"}
@@ -510,7 +510,7 @@ end)("fatih/vim-go");
   vg.ale_enabled = 0
   vg.ale_disable_lsp = 1
   vg.ale_set_highlights = 0
-  vg.ale_echo_msg_format = "%severity% ⏽ %linter% ⏽ %s (%code%)"
+  vg.ale_echo_msg_format = "%severity% ⏽ %linter% ⏽ %code% ⏽ %s"
 
   vg.ale_sign_column_always = 1
   vg.ale_sign_error = ""
