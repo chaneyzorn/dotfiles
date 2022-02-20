18047e67b0d7afd236941f0216a21ead04824a92
chaney <chaneyzorn@gmail.com>
2022-02-20 23:41:05 +0800

    nvim: add config for neovide

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,4 +1,5 @@
 -- init.lua
 
 require("neo.setting")
+require("neo.neovide")
 require("neo.plugins").ensure_packer().setup()
---
+++ nvim/.config/nvim/lua/neo/neovide.lua
@@ -0,0 +1,6 @@
+-- https://github.com/neovide/neovide
+
+vim.o.guifont = "JetBrains Mono NL,Symbols Nerd Font:h10"
+vim.g.neovide_cursor_animation_length = 0.01
+vim.g.neovide_cursor_trail_length = 0.1
+vim.g.neovide_cursor_vfx_mode = "railgun"
