199099fe012c5d5353d35505e897b3b4cd85d61c
chaney <chaneyzorn@gmail.com>
2022-02-25 12:58:52 +0800

    nvim: update neovide config

--- nvim/.config/nvim/lua/neo/neovide.lua
+++ nvim/.config/nvim/lua/neo/neovide.lua
@@ -1,6 +1,8 @@
 -- https://github.com/neovide/neovide
 
 vim.o.guifont = "JetBrains Mono NL,Symbols Nerd Font:h10"
+vim.g.neovide_fullscreen = false
 vim.g.neovide_cursor_animation_length = 0.01
 vim.g.neovide_cursor_trail_length = 0.1
+vim.g.neovide_transparency = 0.92
 vim.g.neovide_cursor_vfx_mode = "railgun"
