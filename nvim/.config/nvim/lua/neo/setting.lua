6346da03e3b1d375d26f5a35576e1ffd20214bc2
chaney <chaneyzorn@gmail.com>
2022-03-22 23:52:00 +0800

    nvim: config eol in listchars

--- nvim/.config/nvim/lua/neo/setting.lua
+++ nvim/.config/nvim/lua/neo/setting.lua
@@ -55,7 +55,7 @@ vo.softtabstop = 4
 
 -- 设置 tab 字符的显示
 vo.list = true
-vo.listchars = "tab:>·"
+vo.listchars = "tab:»·,trail:∙,precedes:←,extends:→,eol:¬,nbsp:␣"
 
 -- 设置窗口分割线为连续实线
 vo.fillchars = "vert:│"
