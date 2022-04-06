cd98b03a5e88aef7de1e05be7a7cda3675aa2a56
chaney <chaneyzorn@gmail.com>
2022-04-06 20:55:23 +0800

    nvim: remove auto_close for new nvim-tree

--- nvim/.config/nvim/lua/neo/plugconf/nvim-tree.lua
+++ nvim/.config/nvim/lua/neo/plugconf/nvim-tree.lua
@@ -31,7 +31,6 @@ function M.post()
     hijack_netrw = true,
     open_on_setup = false,
     ignore_ft_on_setup = {},
-    auto_close = true,
     open_on_tab = false,
     update_to_buf_dir = {
       enable = true,
