fa637f05cbb468595cd4822ff11e9f7764142c06
chaney <chaneyzorn@gmail.com>
2022-06-12 19:20:25 +0800

    nvim: update nvim colorscheme for kanagawa

--- nvim/.config/nvim/lua/neo/plugconf/colorscheme.lua
+++ nvim/.config/nvim/lua/neo/plugconf/colorscheme.lua
@@ -52,12 +52,12 @@ end
 local config_scheme = function()
   require("kanagawa").setup({
     undercurl = true,
-    commentStyle = "italic",
-    functionStyle = "NONE",
-    keywordStyle = "bold",
-    statementStyle = "bold",
-    typeStyle = "NONE",
-    variablebuiltinStyle = "italic",
+    commentStyle = { italic = true },
+    functionStyle = {},
+    keywordStyle = { bold = true },
+    statementStyle = { bold = true },
+    typeStyle = {},
+    variablebuiltinStyle = { italic = true },
     specialReturn = true,
     specialException = true,
     transparent = false,
