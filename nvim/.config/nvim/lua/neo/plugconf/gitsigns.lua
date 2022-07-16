71efa25d4d4c5ed6806b463f4f5ba6b98ead52e9
chaney <chaneyzorn@gmail.com>
2022-07-16 21:32:13 +0800

    nvim: format gitsigns conf with stylua

--- nvim/.config/nvim/lua/neo/plugconf/gitsigns.lua
+++ nvim/.config/nvim/lua/neo/plugconf/gitsigns.lua
@@ -5,11 +5,36 @@ function M.pre() end
 function M.post()
   require("gitsigns").setup({
     signs = {
-      add = { hl = "GitSignsAdd", text = "│", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-      change = { hl = "GitSignsChange", text = "│", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-      delete = { hl = "GitSignsDelete", text = "_", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-      topdelete = { hl = "GitSignsDelete", text = "", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-      changedelete = { hl = "GitSignsChange", text = "│", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+      add = {
+        hl = "GitSignsAdd",
+        text = "│",
+        numhl = "GitSignsAddNr",
+        linehl = "GitSignsAddLn",
+      },
+      change = {
+        hl = "GitSignsChange",
+        text = "│",
+        numhl = "GitSignsChangeNr",
+        linehl = "GitSignsChangeLn",
+      },
+      delete = {
+        hl = "GitSignsDelete",
+        text = "_",
+        numhl = "GitSignsDeleteNr",
+        linehl = "GitSignsDeleteLn",
+      },
+      topdelete = {
+        hl = "GitSignsDelete",
+        text = "",
+        numhl = "GitSignsDeleteNr",
+        linehl = "GitSignsDeleteLn",
+      },
+      changedelete = {
+        hl = "GitSignsChange",
+        text = "│",
+        numhl = "GitSignsChangeNr",
+        linehl = "GitSignsChangeLn",
+      },
     },
   })
 end
