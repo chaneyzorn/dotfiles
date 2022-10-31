0bb781003add760bd2b57e08e796ce94922a6a0b
chaney <chaneyzorn@gmail.com>
2022-10-31 23:11:35 +0800

    terminal: change font to jetbrains moon

--- nvim/.config/nvim/lua/neo/neovide.lua
+++ nvim/.config/nvim/lua/neo/neovide.lua
@@ -1,6 +1,11 @@
 -- https://github.com/neovide/neovide
 
-vim.o.guifont = "JetBrains Mono NL,Symbols Nerd Font:h10"
+if vim.loop.os_uname().sysname == "Darwin" then
+  vim.o.guifont = "JetBrains Mono,Symbols Nerd Font:h14"
+else
+  vim.o.guifont = "JetBrains Mono,Symbols Nerd Font:h10"
+end
+
 vim.g.neovide_fullscreen = false
 vim.g.neovide_cursor_animation_length = 0.01
 vim.g.neovide_cursor_trail_length = 0.1
