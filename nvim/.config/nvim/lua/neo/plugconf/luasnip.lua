9374d4548152622f31387e8f2e46976633a93e37
chaney <chaneyzorn@gmail.com>
2023-03-26 22:08:01 +0800

    nvim: improve startup speed with lazy.nvim

--- nvim/.config/nvim/lua/neo/plugconf/luasnip.lua
+++ nvim/.config/nvim/lua/neo/plugconf/luasnip.lua
@@ -3,7 +3,7 @@ local M = {}
 function M.pre() end
 
 function M.post()
-  require("luasnip.loaders.from_vscode").load()
+  require("luasnip.loaders.from_vscode").lazy_load()
 end
 
 function M.keybind() end
