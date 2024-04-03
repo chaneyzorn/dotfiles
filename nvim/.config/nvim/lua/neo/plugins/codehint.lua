a3085fd068bc86372f6952442bb8cbb47914ee59
chaney <chaneyzorn@gmail.com>
2024-04-03 21:31:22 +0800

    nvim: remoce leap

--- nvim/.config/nvim/lua/neo/plugins/codehint.lua
+++ nvim/.config/nvim/lua/neo/plugins/codehint.lua
@@ -5,39 +5,6 @@ return {
     opts = {},
   },
   {
-    "ggandor/leap.nvim",
-    keys = {
-      { "<M-j>", "<Plug>(leap-forward)", mode = { "n", "x", "o" }, desc = "Leap forward to" },
-      { "<M-k>", "<Plug>(leap-backward)", mode = { "n", "x", "o" }, desc = "Leap backward to" },
-      { "<M-f>", "<Plug>(leap-from-window)", mode = { "n", "x", "o" }, desc = "Leap from windows" },
-    },
-    config = function()
-      local leap_lualine = vim.api.nvim_create_augroup("LeapLualine", { clear = true })
-      vim.api.nvim_create_autocmd("User", {
-        group = leap_lualine,
-        pattern = "LeapEnter",
-        callback = function()
-          vim.g.u_leap_enter = true
-          require("lualine").refresh({
-            scope = "tabpage",
-            place = { "statusline" },
-          })
-        end,
-      })
-      vim.api.nvim_create_autocmd("User", {
-        group = leap_lualine,
-        pattern = "LeapLeave",
-        callback = function()
-          vim.g.u_leap_enter = false
-          require("lualine").refresh({
-            scope = "tabpage",
-            place = { "statusline" },
-          })
-        end,
-      })
-    end,
-  },
-  {
     "folke/flash.nvim",
     event = "VeryLazy",
     opts = {},
