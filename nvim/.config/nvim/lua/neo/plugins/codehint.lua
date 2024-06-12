5991619ad66b958f1ac97f00883365f9eb253554
chaney <chaneyzorn@gmail.com>
2024-06-12 09:46:54 +0800

    nvim: disable ts-context default

--- nvim/.config/nvim/lua/neo/plugins/codehint.lua
+++ nvim/.config/nvim/lua/neo/plugins/codehint.lua
@@ -101,6 +101,19 @@ return {
       "nvim-treesitter/nvim-treesitter-textobjects",
     },
     config = function()
+      require("treesitter-context").setup({
+        enable = false,
+        max_lines = 0,
+        min_window_height = 0,
+        line_numbers = true,
+        multiline_threshold = 20,
+        trim_scope = "outer",
+        mode = "cursor",
+        separator = nil,
+        zindex = 20,
+        on_attach = nil,
+      })
+
       require("nvim-treesitter.configs").setup({
         ensure_installed = "all",
         highlight = {
