ba0c546342741548a0bd5795bafc99901cd0bc9d
chaney <chaneyzorn@gmail.com>
2024-07-17 09:31:39 +0800

    nvim: show current fold only

--- nvim/.config/nvim/lua/neo/plugins/foldent.lua
+++ nvim/.config/nvim/lua/neo/plugins/foldent.lua
@@ -17,6 +17,7 @@ return {
     "gh-liu/fold_line.nvim",
     event = "VeryLazy",
     init = function()
+      vim.g.fold_line_current_fold_only = true
       vim.g.fold_line_char_open_start = "╭"
       vim.g.fold_line_char_open_end = "╰"
     end,
