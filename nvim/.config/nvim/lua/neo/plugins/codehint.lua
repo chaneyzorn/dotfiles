779ee113fba9b00ee7f2114a532c5b86bbb80130
chaney <chaneyzorn@gmail.com>
2025-05-07 15:34:07 +0800

    feat(nvim): treesitter indent is mad, not use it

--- nvim/.config/nvim/lua/neo/plugins/codehint.lua
+++ nvim/.config/nvim/lua/neo/plugins/codehint.lua
@@ -112,7 +112,6 @@ return {
           enable = true,
           additional_vim_regex_highlighting = false,
         },
-        indent = { enable = true },
         incremental_selection = {
           enable = true,
           keymaps = {
