b6de808ffb3f6233db11eed9e361df7e9f53baa6
chaney <chaneyzorn@gmail.com>
2026-01-12 10:40:29 +0800

    feat(nvim): config current_line_blame_formatter_nc

--- nvim/.config/nvim/lua/neo/plugins/git.lua
+++ nvim/.config/nvim/lua/neo/plugins/git.lua
@@ -20,6 +20,7 @@ return {
       require("gitsigns").setup({
         current_line_blame = true,
         current_line_blame_formatter = "      <author_time:%Y-%m-%d>   <author>   <summary>",
+        current_line_blame_formatter_nc = "    󰃉 <author>",
         signs = {
           add = { text = "│" },
           change = { text = "│" },
