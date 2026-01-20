b8e8ca2b132157b6740025292b7de0aec01aa12a
chaney <chaneyzorn@gmail.com>
2026-01-20 15:25:29 +0800

    feat(niri,nvim): use caps lock as ctrl

--- nvim/.config/nvim/lua/neo/plugins/statine.lua
+++ nvim/.config/nvim/lua/neo/plugins/statine.lua
@@ -5,214 +5,240 @@ return {
     dependencies = {
       "nvim-tree/nvim-web-devicons",
     },
-    opts = {
-      options = {
-        globalstatus = true,
-        component_separators = "",
-        section_separators = "",
-        disabled_filetypes = {
-          winbar = { "NvimTree", "aerial", "qf", "trouble" },
-        },
-      },
-      extensions = { "aerial" },
-      sections = {
-        lualine_a = {
-          {
-            function()
-              local get_mode = require("lualine.components.mode")
-              local mode_code = get_mode()
-              local mt = {
-                ["COMMAND"] = "",
-                ["CONFIRM"] = "",
-                ["EX"] = "󱤵",
-                ["INSERT"] = "",
-                ["MORE"] = "󰍻",
-                ["NORMAL"] = "",
-                ["O-PENDING"] = "󰸻",
-                ["REPLACE"] = "",
-                ["S-BLOCK"] = "󰩬",
-                ["S-LINE"] = "",
-                ["SELECT"] = "󰫙",
-                ["SHELL"] = "",
-                ["TERMINAL"] = "",
-                ["V-BLOCK"] = "󰮔",
-                ["V-LINE"] = "󰡫",
-                ["V-REPLACE"] = "󱥼",
-                ["VISUAL"] = "󰈈",
-              }
-              if mt[mode_code] == nil then
-                return mode_code
-              end
-              return mt[mode_code]
-            end,
-            separator = { right = vim.g.neovide and "" or "" },
-            -- separator = { left = vim.g.neovide and "" or "" },
+    config = function()
+      require("lualine").setup({
+        options = {
+          globalstatus = true,
+          component_separators = "",
+          section_separators = "",
+          disabled_filetypes = {
+            winbar = { "NvimTree", "aerial", "qf", "trouble" },
           },
         },
-        lualine_b = {
-          {
-            "branch",
-            icon = "",
+        extensions = { "aerial" },
+        sections = {
+          lualine_a = {
+            {
+              function()
+                local get_mode = require("lualine.components.mode")
+                local mode_code = get_mode()
+                local mt = {
+                  ["COMMAND"] = "",
+                  ["CONFIRM"] = "",
+                  ["EX"] = "󱤵",
+                  ["INSERT"] = "",
+                  ["MORE"] = "󰍻",
+                  ["NORMAL"] = "",
+                  ["O-PENDING"] = "󰸻",
+                  ["REPLACE"] = "",
+                  ["S-BLOCK"] = "󰩬",
+                  ["S-LINE"] = "",
+                  ["SELECT"] = "󰫙",
+                  ["SHELL"] = "",
+                  ["TERMINAL"] = "",
+                  ["V-BLOCK"] = "󰮔",
+                  ["V-LINE"] = "󰡫",
+                  ["V-REPLACE"] = "󱥼",
+                  ["VISUAL"] = "󰈈",
+                }
+                if mt[mode_code] == nil then
+                  return mode_code
+                end
+                return mt[mode_code]
+              end,
+              separator = { right = vim.g.neovide and "" or "" },
+              -- separator = { left = vim.g.neovide and "" or "" },
+            },
           },
-          {
-            "diff",
-            symbols = { added = " ", modified = " ", removed = " " },
+          lualine_b = {
+            {
+              "branch",
+              icon = "",
+            },
+            {
+              "diff",
+              symbols = { added = " ", modified = " ", removed = " " },
+            },
           },
-        },
-        lualine_c = {
-          {
-            "filetype",
-            padding = { left = 1, right = 0 },
-            colored = true,
-            icon_only = true,
+          lualine_c = {
+            {
+              "filetype",
+              padding = { left = 1, right = 0 },
+              colored = true,
+              icon_only = true,
+            },
+            {
+              "filename",
+              path = 1,
+              padding = { left = 0, right = 1 },
+              symbols = {
+                modified = "󰃉 ",
+                readonly = " ",
+                unnamed = "[No Name]",
+                newfile = "[New]",
+              },
+            },
           },
-          {
-            "filename",
-            path = 1,
-            padding = { left = 0, right = 1 },
-            symbols = {
-              modified = "󰃉 ",
-              readonly = " ",
-              unnamed = "[No Name]",
-              newfile = "[New]",
+          lualine_x = {
+            {
+              "encoding",
+              icon = "",
+            },
+            {
+              "fileformat",
+              symbols = {
+                unix = "󰌽 LF",
+                dos = " CRLF",
+                mac = " CR",
+              },
+            },
+            {
+              function()
+                return vim.bo.shiftwidth
+              end,
+              icon = "",
+            },
+            { "filetype" },
+            {
+              "lsp_status",
+              icon = "",
+              ignore_lsp = { "typos_lsp" },
+            },
+            {
+              function()
+                if vim.uv.os_uname().sysname ~= "Linux" then
+                  return ""
+                end
+
+                local wildcard_path = "/sys/class/leds/input*::capslock/brightness"
+                local brightness_path = vim.fn.glob(wildcard_path)
+                if not brightness_path or brightness_path == "" then
+                  return ""
+                end
+                local file, err = io.open(brightness_path, "r")
+                if not file then
+                  return ""
+                end
+
+                local brightness_value = file:read("*a")
+                file:close()
+
+                local is_enabled = brightness_value:gsub("%s+", "") == "1"
+                return is_enabled and "" or ""
+              end,
+              icon = "󰪛",
+            },
+            {
+              "tabs",
+              mode = 1,
+              show_modified_status = false,
+              fmt = function(name, ctx)
+                return string.format("%s %s", "", ctx.tabnr)
+              end,
+            },
+          },
+          lualine_y = {
+            {
+              function()
+                return vim.fs.basename(vim.fn.getcwd())
+              end,
+              icon = "󱃷",
+            },
+          },
+          lualine_z = {
+            {
+              function()
+                return vim.uv.os_gethostname()
+              end,
+              cond = function()
+                return os.getenv("SSH_CONNECTION") ~= nil
+              end,
+              icon = "󰒍",
+              separator = { left = "" },
             },
           },
         },
-        lualine_x = {
-          {
-            "encoding",
-            icon = "",
-          },
-          {
-            "fileformat",
-            symbols = {
-              unix = "󰌽 LF",
-              dos = " CRLF",
-              mac = " CR",
+        winbar = {
+          lualine_a = {},
+          lualine_b = {
+            {
+              "filetype",
+              separator = "",
+              padding = { left = 1, right = 0 },
+              colored = true,
+              icon_only = true,
+            },
+            {
+              "filename",
+              padding = { left = 0, right = 1 },
+              separator = ">",
+              symbols = {
+                modified = "󰃉 ",
+                readonly = " ",
+                unnamed = "[No Name]",
+                newfile = "[New]",
+              },
+            },
+            {
+              "aerial",
+              sep = " > ",
+              depth = 3,
+              dense = false,
+              dense_sep = ".",
+              colored = true,
             },
           },
-          {
-            function()
-              return vim.bo.shiftwidth
-            end,
-            icon = "",
-          },
-          { "filetype" },
-          {
-            "lsp_status",
-            icon = "",
-            ignore_lsp = { "typos_lsp" },
-          },
-          {
-            "tabs",
-            mode = 1,
-            show_modified_status = false,
-            fmt = function(name, ctx)
-              return string.format("%s %s", "", ctx.tabnr)
-            end,
-          },
-        },
-        lualine_y = {
-          {
-            function()
-              return vim.fs.basename(vim.fn.getcwd())
-            end,
-            icon = "󱃷",
-          },
-        },
-        lualine_z = {
-          {
-            function()
-              return vim.uv.os_gethostname()
-            end,
-            cond = function()
-              return os.getenv("SSH_CONNECTION") ~= nil
-            end,
-            icon = "󰒍",
-            separator = { left = "" },
-          },
-        },
-      },
-      winbar = {
-        lualine_a = {},
-        lualine_b = {
-          {
-            "filetype",
-            separator = "",
-            padding = { left = 1, right = 0 },
-            colored = true,
-            icon_only = true,
-          },
-          {
-            "filename",
-            padding = { left = 0, right = 1 },
-            separator = ">",
-            symbols = {
-              modified = "󰃉 ",
-              readonly = " ",
-              unnamed = "[No Name]",
-              newfile = "[New]",
+          lualine_c = {},
+          lualine_x = {},
+          lualine_y = {
+            {
+              "diagnostics",
+              sources = { "nvim_diagnostic" },
+              symbols = { error = " ", warn = " ", info = " ", hint = "󰛨 " },
+            },
+            {
+              "searchcount",
+              icon = "",
+            },
+            {
+              function()
+                local cur = vim.fn.line(".")
+                local total = vim.fn.line("$")
+                local percentage = math.floor(cur / total * 100)
+                return string.format(" %d/%d %d%s", cur, total, percentage, "%%")
+              end,
             },
           },
-          {
-            "aerial",
-            sep = " > ",
-            depth = 3,
-            dense = false,
-            dense_sep = ".",
-            colored = true,
-          },
+          lualine_z = {},
         },
-        lualine_c = {},
-        lualine_x = {},
-        lualine_y = {
-          {
-            "diagnostics",
-            sources = { "nvim_diagnostic" },
-            symbols = { error = " ", warn = " ", info = " ", hint = "󰛨 " },
-          },
-          {
-            "searchcount",
-            icon = "",
-          },
-          {
-            function()
-              local cur = vim.fn.line(".")
-              local total = vim.fn.line("$")
-              local percentage = math.floor(cur / total * 100)
-              return string.format(" %d/%d %d%s", cur, total, percentage, "%%")
-            end,
-          },
-        },
-        lualine_z = {},
-      },
-      inactive_winbar = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = {
-          {
-            "filetype",
-            separator = "",
-            padding = { left = 1, right = 0 },
-            colored = true,
-            icon_only = true,
-          },
-          {
-            "filename",
-            path = 1,
-            padding = { left = 0, right = 1 },
-            symbols = {
-              modified = "󰃉 ",
-              readonly = " ",
-              unnamed = "[No Name]",
-              newfile = "[New]",
+        inactive_winbar = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = {
+            {
+              "filetype",
+              separator = "",
+              padding = { left = 1, right = 0 },
+              colored = true,
+              icon_only = true,
+            },
+            {
+              "filename",
+              path = 1,
+              padding = { left = 0, right = 1 },
+              symbols = {
+                modified = "󰃉 ",
+                readonly = " ",
+                unnamed = "[No Name]",
+                newfile = "[New]",
+              },
             },
           },
+          lualine_x = {},
+          lualine_y = {},
+          lualine_z = {},
         },
-        lualine_x = {},
-        lualine_y = {},
-        lualine_z = {},
-      },
-    },
+      })
+    end,
   },
 }
