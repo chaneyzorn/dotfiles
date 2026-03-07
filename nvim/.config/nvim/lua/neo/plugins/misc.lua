47e8bb4b460f100504a5397060e788cf973e4779
chaney <chaneyzorn@gmail.com>
2026-03-08 00:54:42 +0800

    feat(nvim): custom filter-do.nvim config

--- nvim/.config/nvim/lua/neo/plugins/misc.lua
+++ nvim/.config/nvim/lua/neo/plugins/misc.lua
@@ -198,39 +198,4 @@ return {
       })
     end,
   },
-  {
-    dir = vim.fn.expand("~/Projects/iself/filter-do.nvim"),
-    dev = true,
-    enabled = function()
-      return vim.uv.fs_stat(vim.fs.abspath("~/Projects/iself/filter-do.nvim"))
-    end,
-    cmd = "Fx",
-    keys = {
-      {
-        "<leader>fx",
-        function()
-          require("filter_do.api").select_filter_do()
-        end,
-        mode = { "n", "v" },
-        desc = "filter-do",
-      },
-    },
-    config = function()
-      require("filter_do").setup({
-        ui = {
-          -- ui_select = "default",
-          -- ui_select = "snacks.picker",
-          -- ui_select = "telescope",
-          -- ui_select = "mini.pick",
-          -- ui_select = "auto",
-        },
-      })
-      vim.api.nvim_create_autocmd("User", {
-        pattern = { "FxUIOpenPost", "FxUIPreviousPost", "FxUINextPost" },
-        callback = function(e)
-          vim.api.nvim_set_option_value("winbar", "", { scope = "local", win = e.data.target_win_id })
-        end,
-      })
-    end,
-  },
 }
